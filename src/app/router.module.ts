f725c89f78fe2557ebd3b8ed0051f862a89310d6
Vino Rajan <vinorajan92@live.com>
2018-03-04 13:20:48 +0530

    commit -m"page not foud component added"

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';     // Add this
 import { AboutComponent } from './about/about.component';     // Add this
 import { ContactComponent } from './contact/contact.component';     // Add this
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';     // Add this
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path: 'home', component: HomeComponent},
+    {path: '', component: HomeComponent},
     {path: 'contact', component: ContactComponent},
-    {path: 'about',component: AboutComponent}
+    {path: 'about',component: AboutComponent},
+    { path: '**', component: PagenotfoundComponent }
   ];
 
   @NgModule({
@@ -19,4 +20,4 @@ const routes: Routes = [
   export class AppRoutingModule{
 
   }
-  export const RoutingComponents = [HomeComponent,AboutComponent,ContactComponent]
+  export const RoutingComponents = [HomeComponent,AboutComponent,ContactComponent,PagenotfoundComponent]
